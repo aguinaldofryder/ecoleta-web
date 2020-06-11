12391969a94fa8144eac0122783b16d86d85c243
Aguinaldo Fryder <aguinaldofryder@hotmail.com>
2020-06-10 21:47:44 -0300

    Alterar URL base

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import dotenv from 'dotenv';
 
-export const urlBase = process.env.BASE_URL || 'http://localhost:4000';
+export const urlBase =
+  process.env.BASE_URL || 'https://ecoletafry-api.herokuapp.com';
 
 const api = axios.create({
   baseURL: urlBase,
